6c20554246e13055fa4b67e4e758f50b647e4b7e
kuring <361118339@qq.com>
2021-08-19 21:37:26 +0800

    chang bin to .local, add color

--- .config/zsh/conf/env.zsh
+++ .config/zsh/conf/env.zsh
@@ -1,11 +1,11 @@
 export LOCALPROG=$HOME/prog
-#export GOPATH=$HOME/go
+export GOPATH="$XDG_DATA_HOME"/go
 
 # path
 export PATH=$PATH:$LOCALBIN
-export PATH=$PATH:$HOME/.gem/ruby/2.7.0
+#export PATH=$PATH:$HOME/.gem/ruby/2.7.0
 export PATH=$PATH:/usr/local/bin
-export PATH=$PATH:$HOME/go/bin
+#export PATH=$PATH:$HOME/go/bin
 export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
 export PATH=$PATH:/home/linuxbrew/.linuxbrew/sbin
 export PATH=$PATH:$HOME/.linuxbrew/bin
@@ -17,11 +17,11 @@ export PATH=$PATH:$HOME/.cargo/bin
 export PATH=$PATH:/snap/bin
 export PATH=$PATH:/usr/local/opt/node@12/bin
 #export TERM=xterm-256color
-export PATH=$PATH:$LOCALPROG/flutter/bin
-export PATH=$PATH:$LOCALPROG/flutter/bin/cache/dart-sdk/bin
-export PATH="$PATH":"$HOME/.pub-cache/bin"
+#export PATH=$PATH:$LOCALPROG/flutter/bin
+#export PATH=$PATH:$LOCALPROG/flutter/bin/cache/dart-sdk/bin
+#export PATH="$PATH":"$HOME/.pub-cache/bin"
 
-export FLUTTER_ROOT=$LOCALPROG/flutter
+#export FLUTTER_ROOT=$LOCALPROG/flutter
 export TERM=xterm-256color
 export TERM_ITALICS=true
 export RANGER_LOAD_DEFAULT_RC="false"

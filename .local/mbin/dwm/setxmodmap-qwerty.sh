8de9dc05195bd077764b9a13d3b032c2bbf7b9e6
kuring <361118339@qq.com>
2021-09-24 14:52:01 +0800

    update

--- .local/mbin/dwm/setxmodmap-qwerty.sh
+++ .local/mbin/dwm/setxmodmap-qwerty.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #setxkbmap us -option caps:swapescape -option ctrl:swap_lalt_lctl -option lv3:ralt_alt && notify-send "Set QWERTY Keyboard"
-setxkbmap us -option -option ctrl:swap_lalt_lctl && notify-send "Set QWERTY Keyboard"
+setxkbmap us -option -option ctrl:swap_lalt_lctl -option caps:swapescape && notify-send "Set QWERTY Keyboard"
+
 xset r rate 250 30
 
 

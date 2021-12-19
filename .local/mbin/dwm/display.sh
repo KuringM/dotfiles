a0c118535ab229188c8dd3e5af4ae1cf9ed4b33b
kuring <361118339@qq.com>
2021-12-19 18:06:35 +0800

    add display.sh

---
+++ .local/mbin/dwm/display.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+optimus-manager --switch nvidia
+systemctl restart sddm.service
+#xrandr --auto --output DP-1 --same-as eDP-1 --size 1920x1080
+#xrandr --auto --output HDMI-1  --right-of DP-2 --size 1920x1080
+xrandr --output DP-2 --mode 2560x1440 --rate 164.69
+xrandr --output eDP-1-1 --off
--- .local/mbin/dwm/dwm-autostart.sh
+++ .local/mbin/dwm/dwm-autostart.sh
@@ -2,14 +2,17 @@
 
 /bin/bash ~/.local/mbin/dwm/dwm-status.sh &
 /bin/bash ~/.local/mbin/dwm/dwm-wp_autochange.sh &
-#picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
-picom -b
 /bin/bash ~/.local/mbin/dwm/dwm-tap_to_click.sh &
 /bin/bash ~/.local/mbin/dwm/dwm-inverse_scroll.sh &
 /bin/bash ~/.local/mbin/dwm/setxmodmap-colemak.sh &
+/bin/bash ~/.local/mbin/dwm/display.sh &
+
+#picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
+picom -b
 nm-applet &
 blueman-applet &
 xfce4-power-manager &
 ~/.local/mbin/dwm/dwm-autostart_wait.sh &
 fcitx5 &
 flameshot &
+optimus-manager-qt &

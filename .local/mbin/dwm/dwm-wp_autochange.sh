4e09c20cf612f3804a359d019b7a8659fcefbb28
kuring <361118339@qq.com>
2021-12-27 16:43:21 +0800

    add bspwm, sxhkbm config; update mbin

--- .local/mbin/dwm/dwm-wp_autochange.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while true; do
-	/bin/bash ~/.local/mbin/dwm/dwm-wp_change.sh
-	sleep 3m
-done
--- .local/mbin/onlyExternalDisplay.sh
+++ .local/mbin/onlyExternalDisplay.sh
@@ -2,5 +2,5 @@
 
 #xrandr --auto --output DP-1 --same-as eDP-1 --size 1920x1080
 #xrandr --auto --output HDMI-1  --right-of DP-2 --size 1920x1080
-xrandr --output DP-2 --mode 2560x1440 --rate 164.69
-xrandr --output eDP-1-1 --off
+
+xrandr --auto --output DP-2 --primary --mode 2560x1440 --rate 164.69 --output eDP-1-1 --off

4241231ace8b95ac3c4ca7d061530d2dfb855a72
kuring <361118339@qq.com>
2022-01-07 11:22:12 +0800

    update

--- .local/mbin/dwm/dwm-autostart.sh
+++ .local/mbin/dwm/dwm-autostart.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 /bin/bash ~/.local/mbin/dwm/dwm-status.sh &
-/bin/bash ~/.local/mbin/dwm/dwm-tap_to_click.sh &
-/bin/bash ~/.local/mbin/dwm/dwm-inverse_scroll.sh &
+/bin/bash ~/.local/mbin/general/tap_to_click.sh &
+/bin/bash ~/.local/mbin/general/inverse_scroll.sh &
 
-/bin/bash ~/.local/mbin/setxmodmap-colemak.sh &
-/bin/bash ~/.local/mbin/onlyExternalDisplay.sh &
-/bin/bash ~/.local/mbin/wp_autochange.sh &
+/bin/bash ~/.local/mbin/general/setxmodmap-colemak.sh &
+/bin/bash ~/.local/mbin/general/onlyExternalDisplay.sh &
+/bin/bash ~/.local/mbin/general/wp_autochange.sh &
 
 #picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 picom -b
---
+++ .local/mbin/general/wp_autochange.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while true; do
+	/bin/bash ~/.local/mbin/general/wp_change.sh
+	sleep 3m
+done

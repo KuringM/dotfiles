d2703e6d3a8b635aafea62426a1781655fa81ad6
Kuring <361118339@qq.com>
2024-09-21 21:30:34 +0800

    update zsh conf

--- .config/zsh/conf/aliases.zsh
+++ .config/zsh/conf/aliases.zsh
@@ -36,5 +36,6 @@ alias ta='tmux a'
 alias t='tmux'
 alias lo='lsof -p $(fps) +w'
 
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME' && compdef config=git
+alias cf='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME' && compdef config=git
+alias cfg='lazygit --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias kk='nvim .gitignore'

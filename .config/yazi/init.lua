8388e9a3568de128e9161a1d0d51e2b1f24a150f
Kuring <361118339@qq.com>
2024-09-26 19:54:25 +0800

    update yazi conf

---
+++ .config/yazi/init.lua
@@ -0,0 +1,70 @@
+require("yaziline"):setup {
+	separator_style = "curvy",
+	select_symbol = "",
+	yank_symbol = "󰆐",
+	filename_max_length = 24, -- trim when filename > 24
+	filename_trim_length = 6 -- trim 6 chars from both ends
+}
+require("starship"):setup {
+	config_file = "~/.config/starship/starship.toml",
+}
+require("git"):setup {}
+
+
+require("yamb"):setup {
+	cli = "fzf",
+  path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark") or
+        (os.getenv("HOME") .. "/.local/share/yazi/bookmark"),
+}
+
+-- Linemode display size anm mtime together
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.modified or 0)
+	if time == 0 then
+		time = ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("%b/%d %H:%M", time)
+	else
+		time = os.date("%b/%d  %Y", time)
+	end
+
+	local size = self._file:size()
+	return ui.Line(string.format("%s %s", size and ya.readable_size(size) or "-", time))
+end
+
+-- Show symlink in status bar
+function Status:name()
+	local h = self._tab.current.hovered
+	if not h then
+		return ui.Line {}
+	end
+
+	local linked = ""
+	if h.link_to ~= nil then
+		linked = " -> " .. tostring(h.link_to)
+	end
+	return ui.Line(" " .. h.name .. linked)
+end
+
+-- Show user/group of files in status bar
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ui.Line {}
+	end
+
+	return ui.Line {
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		ui.Span(":"),
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		ui.Span(" "),
+	}
+end, 500, Status.RIGHT)
+
+-- Show username and hostname in header
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ui.Line {}
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)

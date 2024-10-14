074b3f0c427127779db1711e42ce29063d0454e5
Kuring <361118339@qq.com>
2024-10-15 01:23:07 +0800

    update

--- .config/zsh/conf/kitty.zsh
+++ .config/zsh/conf/kitty.zsh
@@ -12,4 +12,5 @@ fi
 # An alternative MacOS application icon for the wonderful Kitty terminal emulator. https://github.com/DinkDonk/kitty-icon.git
 if [[ ! -e $XDG_CONFIG_HOME/kitty/kitty.app.icns ]]; then
 	ln -s $XDG_CONFIG_HOME/kitty/themes/kitty-light.icns $XDG_CONFIG_HOME/kitty/kitty.app.icns
+	rm /var/folders/*/*/*/com.apple.dock.iconcache; killall Dock
 fi

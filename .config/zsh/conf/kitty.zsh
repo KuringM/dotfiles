c2d79b10916111d420ae2d9ed82102000223106c
Kuring <361118339@qq.com>
2024-10-15 01:22:24 +0800

    update

--- .config/zsh/conf/kitty.zsh
+++ .config/zsh/conf/kitty.zsh
@@ -8,3 +8,8 @@ if [[ -n $KITTY_INSTALLATION_DIR ]]; then
   source -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty.zsh
 fi
 # unset KITTY_SHELL_INTEGRATION
+
+# An alternative MacOS application icon for the wonderful Kitty terminal emulator. https://github.com/DinkDonk/kitty-icon.git
+if [[ ! -e $XDG_CONFIG_HOME/kitty/kitty.app.icns ]]; then
+	ln -s $XDG_CONFIG_HOME/kitty/themes/kitty-light.icns $XDG_CONFIG_HOME/kitty/kitty.app.icns
+fi
